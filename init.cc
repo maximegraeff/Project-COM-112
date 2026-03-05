0c456a87f2ad795155d285eadfb0daf63e922478
maximegraeff <maxime_graeff@yahoo.com>
2026-03-05 15:53:21 +0100

    Adding files

---
+++ init.cc
@@ -0,0 +1,6 @@
+// init.cc  : # fonctions qui initialisent les objects en focntion des données passées en argument par le fichier read.cc           
+//
+//
+// Version 1.0 du 05.03.2025
+//
+
---
+++ read.cc
@@ -0,0 +1,7 @@
+// read.cc  : # fonctions qui lisent le fichier passé en entrée, qui verifie si les données sont conformes aux 
+//            spécificaitons et qui appelle les fonctions de init.cc pour créer les objets.            
+//
+//
+// Version 1.0 du 05.03.2025
+//
+
